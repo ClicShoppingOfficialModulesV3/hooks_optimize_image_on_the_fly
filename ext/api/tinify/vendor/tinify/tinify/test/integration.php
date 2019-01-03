a224cc052fbdcd94b0547d9cbd4c30ce33e4eea1
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-01-03 12:02:53 +0100

    update

--- ext/api/tinify/vendor/tinify/tinify/test/integration.php
+++ ext/api/tinify/vendor/tinify/tinify/test/integration.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ *
+ *  @copyright 2008 - https://www.clicshopping.org
+ *  @Brand : ClicShopping(Tm) at Inpi all right Reserved
+ *  @Licence GPL 2 & MIT
+ *  @licence MIT - Portion of osCommerce 2.4
+ *  @Info : https://www.clicshopping.org/forum/trademark/
+ *
+ */
 
 if (!getenv("TINIFY_KEY")) {
     exit("Set the TINIFY_KEY environment variable.\n");
